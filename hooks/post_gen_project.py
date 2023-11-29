b2f1905f9d114ef102ce7476827a1d9d1dc55ad1
Ben Trachtenberg <btr1975@gmail.com>
2023-11-29 17:36:41 -0600

    update post_gen_hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,14 +1,31 @@
 """
 post generation hooks for cookiecutter to remove unneeded files
 """
+from typing import List
 import os
 
-REMOVE_PATHS = [
-    '{% if cookiecutter.include_cli != "y" %}{{cookiecutter.__library_name}}/{{cookiecutter.__library_name}}_cli.py{% endif %}',
+REMOVE_PATHS_NOT_CLI = [
+    '{% if cookiecutter.include_cli != "y" %}{{cookiecutter.__library_name}}/'
+    '{{cookiecutter.__library_name}}_cli.py{% endif %}',
     '{% if cookiecutter.include_cli != "y" %}{{cookiecutter.__library_name}}/example.py{% endif %}',
 ]
 
-for path in REMOVE_PATHS:
-    path = path.strip()
-    if path and os.path.exists(path):
-        os.unlink(path) if os.path.isfile(path) else os.rmdir(path)
+
+def remove_paths(paths_to_remove: List[str]) -> None:
+    """Remove files and directories
+
+    :rtype: None
+    :returns: Nothing it removes files and directories
+    """
+    for remove_path in paths_to_remove:
+        path = remove_path.strip()
+        if path and os.path.exists(path):
+            if os.path.isfile(path):
+                os.remove(path)
+
+            elif os.path.isdir(path):
+                os.rmdir(path)
+
+
+if __name__ == '__main__':
+    remove_paths(REMOVE_PATHS_NOT_CLI)
