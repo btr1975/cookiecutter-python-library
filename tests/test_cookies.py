c630ff98b07666739812fce9a20f186adb7c956c
Ben <btr1975@gmail.com>
2023-12-14 14:19:06 -0600

    fixed tests for the 'new' format

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -3,12 +3,12 @@ def test_bake_project_no_cli(cookies, bake_project_no_cli_data):
 
     assert result.exit_code == 0
     assert result.exception is None
-    assert result.project.basename == 'python-no-cli'
-    assert result.project.isdir()
-    assert result.project.join('README.md').isfile()
-    assert result.project.join('python_no_cli').isdir()
-    assert not result.project.join('python_no_cli', 'python_no_cli_cli.py').isfile()
-    assert not result.project.join('python_no_cli', 'example.py').isfile()
+    assert result.project_path.name == 'python-no-cli'
+    assert result.project_path.is_dir()
+    assert result.project_path.joinpath('README.md').is_file()
+    assert result.project_path.joinpath('python_no_cli').is_dir()
+    assert not result.project_path.joinpath('python_no_cli', 'python_no_cli_cli.py').is_file()
+    assert not result.project_path.joinpath('python_no_cli', 'example.py').is_file()
 
 
 def test_bake_project_cli(cookies, bake_project_cli_data):
@@ -16,9 +16,9 @@ def test_bake_project_cli(cookies, bake_project_cli_data):
 
     assert result.exit_code == 0
     assert result.exception is None
-    assert result.project.basename == 'python-with-cli'
-    assert result.project.isdir()
-    assert result.project.join('README.md').isfile()
-    assert result.project.join('python_with_cli').isdir()
-    assert result.project.join('python_with_cli', 'python_with_cli_cli.py').isfile()
-    assert result.project.join('python_with_cli', 'example.py').isfile()
+    assert result.project_path.name == 'python-with-cli'
+    assert result.project_path.is_dir()
+    assert result.project_path.joinpath('README.md').is_file()
+    assert result.project_path.joinpath('python_with_cli').is_dir()
+    assert result.project_path.joinpath('python_with_cli', 'python_with_cli_cli.py').is_file()
+    assert result.project_path.joinpath('python_with_cli', 'example.py').is_file()
