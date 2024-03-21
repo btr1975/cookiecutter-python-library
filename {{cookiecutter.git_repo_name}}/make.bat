b2a616169cfbf157901029306d50c61b84eb4647
Ben <btr1975@gmail.com>
2024-03-21 15:53:04 -0500

    added black for formatting

--- {{cookiecutter.git_repo_name}}/make.bat
+++ {{cookiecutter.git_repo_name}}/make.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 REM Makefile for project needs
 REM Author: Ben Trachtenberg
-REM Version: 1.0.4
+REM Version: 1.0.5
 REM
 
 IF "%1" == "build" (
@@ -24,6 +24,11 @@ IF "%1" == "pytest" (
     GOTO END
 )
 
+IF "%1" == "format" (
+    black {{cookiecutter.__library_name}}/
+    GOTO END
+)
+
 {% if cookiecutter.library_documents_location == 'github-pages' %}
 IF "%1" == "gh-pages" (
     rmdir /s /q docs\source\code
@@ -36,6 +41,7 @@ IF "%1" == "gh-pages" (
 @ECHO make options
 @ECHO     build     To build a distribution
 @ECHO     coverage  To run coverage and display ASCII and output to htmlcov
+@ECHO     format    To format the code with black
 @ECHO     pylint    To run pylint
 @ECHO     pytest    To run pytest with verbose option
 {% if cookiecutter.library_documents_location == 'github-pages' %}@ECHO     gh-pages  To create the GitHub pages{% endif %}
