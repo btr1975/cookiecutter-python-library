3ff7cb5562f8839527c41dbd8d7d3f4b5b4dcfae
Ben Trachtenberg <e_ben_75-python@yahoo.com>
2025-04-25 16:03:28 -0500

    updated reqs del for uv

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -6,6 +6,25 @@ def test_bake_project_no_cli_podman(cookies, bake_project_no_cli_podman_data):
     assert result.project_path.name == "python-no-cli"
     assert result.project_path.is_dir()
     assert result.project_path.joinpath("README.md").is_file()
+    assert result.project_path.joinpath("requirements.txt").is_file()
+    assert result.project_path.joinpath("requirements-dev.txt").is_file()
+    assert result.project_path.joinpath("python_no_cli").is_dir()
+    assert not result.project_path.joinpath("python_no_cli", "python_no_cli_cli.py").is_file()
+    assert not result.project_path.joinpath("python_no_cli", "example.py").is_file()
+    assert result.project_path.joinpath("containers", "Containerfile").is_file()
+    assert not result.project_path.joinpath("containers", "Dockerfile").is_file()
+
+
+def test_bake_project_uv_no_cli_podman(cookies, bake_project_uv_no_cli_podman_data):
+    result = cookies.bake(extra_context=bake_project_uv_no_cli_podman_data)
+
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project_path.name == "python-no-cli"
+    assert result.project_path.is_dir()
+    assert result.project_path.joinpath("README.md").is_file()
+    assert not result.project_path.joinpath("requirements.txt").is_file()
+    assert not result.project_path.joinpath("requirements-dev.txt").is_file()
     assert result.project_path.joinpath("python_no_cli").is_dir()
     assert not result.project_path.joinpath("python_no_cli", "python_no_cli_cli.py").is_file()
     assert not result.project_path.joinpath("python_no_cli", "example.py").is_file()
@@ -21,6 +40,25 @@ def test_bake_project_no_cli_docker(cookies, bake_project_no_cli_docker_data):
     assert result.project_path.name == "python-no-cli"
     assert result.project_path.is_dir()
     assert result.project_path.joinpath("README.md").is_file()
+    assert result.project_path.joinpath("requirements.txt").is_file()
+    assert result.project_path.joinpath("requirements-dev.txt").is_file()
+    assert result.project_path.joinpath("python_no_cli").is_dir()
+    assert not result.project_path.joinpath("python_no_cli", "python_no_cli_cli.py").is_file()
+    assert not result.project_path.joinpath("python_no_cli", "example.py").is_file()
+    assert not result.project_path.joinpath("containers", "Containerfile").is_file()
+    assert result.project_path.joinpath("containers", "Dockerfile").is_file()
+
+
+def test_bake_project_uv_no_cli_docker(cookies, bake_project_uv_no_cli_docker_data):
+    result = cookies.bake(extra_context=bake_project_uv_no_cli_docker_data)
+
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project_path.name == "python-no-cli"
+    assert result.project_path.is_dir()
+    assert result.project_path.joinpath("README.md").is_file()
+    assert not result.project_path.joinpath("requirements.txt").is_file()
+    assert not result.project_path.joinpath("requirements-dev.txt").is_file()
     assert result.project_path.joinpath("python_no_cli").is_dir()
     assert not result.project_path.joinpath("python_no_cli", "python_no_cli_cli.py").is_file()
     assert not result.project_path.joinpath("python_no_cli", "example.py").is_file()
@@ -36,6 +74,25 @@ def test_bake_project_cli_podman(cookies, bake_project_cli_podman_data):
     assert result.project_path.name == "python-with-cli"
     assert result.project_path.is_dir()
     assert result.project_path.joinpath("README.md").is_file()
+    assert result.project_path.joinpath("requirements.txt").is_file()
+    assert result.project_path.joinpath("requirements-dev.txt").is_file()
+    assert result.project_path.joinpath("python_with_cli").is_dir()
+    assert result.project_path.joinpath("python_with_cli", "python_with_cli_cli.py").is_file()
+    assert result.project_path.joinpath("python_with_cli", "example.py").is_file()
+    assert result.project_path.joinpath("containers", "Containerfile").is_file()
+    assert not result.project_path.joinpath("containers", "Dockerfile").is_file()
+
+
+def test_bake_project_uv_cli_podman(cookies, bake_project_uv_cli_podman_data):
+    result = cookies.bake(extra_context=bake_project_uv_cli_podman_data)
+
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project_path.name == "python-with-cli"
+    assert result.project_path.is_dir()
+    assert result.project_path.joinpath("README.md").is_file()
+    assert not result.project_path.joinpath("requirements.txt").is_file()
+    assert not result.project_path.joinpath("requirements-dev.txt").is_file()
     assert result.project_path.joinpath("python_with_cli").is_dir()
     assert result.project_path.joinpath("python_with_cli", "python_with_cli_cli.py").is_file()
     assert result.project_path.joinpath("python_with_cli", "example.py").is_file()
@@ -51,6 +108,25 @@ def test_bake_project_cli_docker(cookies, bake_project_cli_docker_data):
     assert result.project_path.name == "python-with-cli"
     assert result.project_path.is_dir()
     assert result.project_path.joinpath("README.md").is_file()
+    assert result.project_path.joinpath("requirements.txt").is_file()
+    assert result.project_path.joinpath("requirements-dev.txt").is_file()
+    assert result.project_path.joinpath("python_with_cli").is_dir()
+    assert result.project_path.joinpath("python_with_cli", "python_with_cli_cli.py").is_file()
+    assert result.project_path.joinpath("python_with_cli", "example.py").is_file()
+    assert not result.project_path.joinpath("containers", "Containerfile").is_file()
+    assert result.project_path.joinpath("containers", "Dockerfile").is_file()
+
+
+def test_bake_project_uv_cli_docker(cookies, bake_project_uv_cli_docker_data):
+    result = cookies.bake(extra_context=bake_project_uv_cli_docker_data)
+
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project_path.name == "python-with-cli"
+    assert result.project_path.is_dir()
+    assert result.project_path.joinpath("README.md").is_file()
+    assert not result.project_path.joinpath("requirements.txt").is_file()
+    assert not result.project_path.joinpath("requirements-dev.txt").is_file()
     assert result.project_path.joinpath("python_with_cli").is_dir()
     assert result.project_path.joinpath("python_with_cli", "python_with_cli_cli.py").is_file()
     assert result.project_path.joinpath("python_with_cli", "example.py").is_file()
